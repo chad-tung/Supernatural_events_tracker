3709f90f148a1c8ad6ad88f4cd0b6be70ee7b27a
chad-tung <chad.tung@gmail.com>
2017-12-14 16:02:15 +0000

    installed mongodb and setup server file

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 var express = require("express");
+var parser = require('body-parser')
 var app = express();
-
+var path = require("path");
+var ObjectId = require("mongodb").ObjectId;
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
 app.use(express.static("client/build"));
+
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/paranormal', function(err, client) {
+	if(err) {
+		console.log(err);
+	}
+	db = client.db("paranormal");
+	console.log("Connected to database.");
+
+	app.listen(3000, function() {
+		console.log("Listening on port 3000");
+	});
+});
 
 app.get("/", function(req, res){
 	res.sendFile(__dirname + "/client/build/index.html");
-});
-
-app.listen(3000, function(){
-	console.log("Listening");
 });
