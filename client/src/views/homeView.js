b152750b0b3e04b2b2f8f9e14dc2c5e16eef848f
chad-tung <chad.tung@gmail.com>
2017-12-15 17:28:47 +0000

    Created a function to make things easier

--- client/src/views/homeView.js
+++ client/src/views/homeView.js
@@ -1,5 +1,31 @@
 var HomeView = function() {
+  this.render();
+}
 
+var elementCreator = function(elementname, idname, classname) {
+  var item = document.createElement(elementname);
+  item.id = idname;
+  item.classList.add(classname);
+}
+
+HomeView.prototype = {
+  render: function() {
+    var homepageDiv = document.getElementById('home-page');
+
+    // var homeimageDiv = document.createElement('div');
+    // homeimageDiv.id = "home-image";
+    var homeimageDiv = elementCreator("div", "home-image", "section first");
+
+    var header = document.createElement('h1');
+    header.innerText = "DISCOVER THE TRUTH";
+
+    homeimageDiv.appendChild(header);
+
+    
+
+    var recent_posts = document.getElementById('recent-posts');
+
+  }
 }
 
 module.exports = HomeView;
