47d9389b1b4f32566025bf6a9fa74607d9a29843
chad-tung <chad.tung@gmail.com>
2017-12-15 14:55:08 +0000

    added id to nav bar post option

--- client/src/app.js
+++ client/src/app.js
@@ -1,3 +1,18 @@
+var FormView = require('./views/formView')
+
+var app = function() {
+	url = "/event-form";
+	makeRequest(url, renderForm);
+}
+
+var makeRequest = function(url, callback) {
+	
+}
+
+var renderForm = function() {
+
+}
+
 window.addEventListener("load", function(){
 	alert("Loaded!");
 });
