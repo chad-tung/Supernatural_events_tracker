964c84dbf94f0ae55e00f9447c34e4d156c72175
chad-tung <chad.tung@gmail.com>
2017-12-15 17:51:41 +0000

    Function works to create element, homepage view sorted

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,9 @@
 var FormView = require('./views/formView');
+var HomeView = require('./views/homeView');
 
 var app = function() {
+	console.log("hi");
+	var home = new HomeView();
 	var form = new FormView();
 }
 
--- client/src/views/homeView.js
+++ client/src/views/homeView.js
@@ -2,31 +2,42 @@ var HomeView = function() {
   this.render();
 }
 
-var elementCreator = function(elementname, idname, classname) {
+var elementCreator = function(elementname, idname, classname, classname2) {
   var item = document.createElement(elementname);
   item.id = idname;
-  item.classList.add(classname);
+  item.classList.add(classname, classname2);
+  return item;
 }
 
 HomeView.prototype = {
   render: function() {
     var homepageDiv = document.getElementById('home-page');
+    var recent_posts = document.getElementById('recent-posts');
 
     // var homeimageDiv = document.createElement('div');
     // homeimageDiv.id = "home-image";
-    var homeimageDiv = elementCreator("div", "home-image", "section first");
 
-    var header = document.createElement('h1');
-    header.innerText = "DISCOVER THE TRUTH";
+    var homeimageDiv = elementCreator("div", "home-image", "section", "first");
 
-    homeimageDiv.appendChild(header);
+    var imageText = document.createElement('h1');
+    imageText.innerText = "DISCOVER THE TRUTH";
 
-    var aboutDiv = elementCreator("div", "about-section", "section second");
+    homeimageDiv.appendChild(imageText);
+
+    var aboutDiv = elementCreator("div", "about-section", "section", "second");
     var aboutHeader = elementCreator('h2');
     aboutHeader.innerText = "ABOUT US";
 
+    var aboutText = elementCreator("div", "", "about-text");
 
-    var recent_posts = document.getElementById('recent-posts');
+    aboutText.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore."
+
+    aboutDiv.appendChild(aboutHeader);
+    aboutDiv.appendChild(aboutText);
+
+    homepageDiv.appendChild(homeimageDiv);
+    homepageDiv.appendChild(aboutDiv);
+    homepageDiv.appendChild(recent_posts);
 
   }
 }
