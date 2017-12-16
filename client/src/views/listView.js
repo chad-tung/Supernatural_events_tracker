2ebd33523920e35eb0e570a5146fdb8cb669a971
Scott Murray <rai.five@gmail.com>
2017-12-16 12:24:07 +0000

    ListView provisionally complete

--- client/src/views/listView.js
+++ client/src/views/listView.js
@@ -1,7 +1,28 @@
 var ListView = function() {
-
-
+  this.render();
 }
 
+
+var elementCreator = function(elementname, idname, classname, classname2, innerString) {
+  var item = document.createElement(elementname);
+  item.id = idname;
+  item.classList.add(classname, classname2);
+  item.innerText = innerString;
+  return item;
+}
+
+ListView.prototype = {
+  render: function(eventList){
+    var eventListPage = document.getElementById('event-list-page');
+    var eventListUL = document.createElement('ul');
+
+    eventListPage.appendChild(eventListUL);
+
+    eventList.forEach(function(event){
+      eventListUL.appendChild(elementCreator('li', `event-${event._id}`, "event-li", "", event.title));
+    })
+
+  }
+}
 
 module.exports = ListView;
