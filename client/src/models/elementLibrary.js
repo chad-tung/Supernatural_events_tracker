9cb3351dde33840759a634f7d4c02087efdbdeb8
chad-tung <chad.tung@gmail.com>
2017-12-16 17:32:48 +0000

    Element creator class fixed, forgot to return the element created. listView file incorporates library

--- client/src/models/elementLibrary.js
+++ client/src/models/elementLibrary.js
@@ -7,12 +7,14 @@ ElementLibrary.prototype = {
     var element = document.createElement(elementName);
     element.id = idName;
     element.classList.add(className, className2);
+    return element;
   },
   elementTextIdClass: function(elementName, innerString, idName, className) {
     var element = document.createElement(elementName);
     element.innerText = innerString;
     element.id = idName;
     element.classList.add(className);
+    return element;
   }
 }
 
--- client/src/views/listView.js
+++ client/src/views/listView.js
@@ -1,6 +1,11 @@
+var ElementLibrary = require('../models/ElementLibrary');
+
+
 var ListView = function(eventList) {
   this.render(eventList);
 }
+
+
 
 
 var elementCreator = function(elementname, idname, classname, innerString) {
@@ -13,12 +18,14 @@ var elementCreator = function(elementname, idname, classname, innerString) {
 
 ListView.prototype = {
   render: function(eventList){
+    var eLib = new ElementLibrary();
     var eventListPage = document.getElementById('event-list-page');
     var eventListUL = document.createElement('ul');
     eventListPage.appendChild(eventListUL);
 
     eventList.forEach(function(event){
-      eventListUL.appendChild(elementCreator('li', `${event._id}`, "event-li", event.title));
+      eventListUL.appendChild(eLib.elementTextIdClass("li", event.title, `${event._id}`, "event-li"));
+      // eventListUL.appendChild(elementCreator('li', `${event._id}`, "event-li", event.title));
     })
 
   }
