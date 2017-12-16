b3bbf0e427aa50229a308b776dc5ecd0c21c1cae
chad-tung <chad.tung@gmail.com>
2017-12-16 17:35:44 +0000

    edited element creator file so it accepts 2 classes

--- client/src/models/elementLibrary.js
+++ client/src/models/elementLibrary.js
@@ -9,11 +9,11 @@ ElementLibrary.prototype = {
     element.classList.add(className, className2);
     return element;
   },
-  elementTextIdClass: function(elementName, innerString, idName, className) {
+  elementTextIdClass: function(elementName, innerString, idName, className, className2) {
     var element = document.createElement(elementName);
     element.innerText = innerString;
     element.id = idName;
-    element.classList.add(className);
+    element.classList.add(className, className2);
     return element;
   }
 }
