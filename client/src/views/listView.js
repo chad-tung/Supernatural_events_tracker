1dde76d231ebf86ee2dd1acdcf32842a4e852fc8
chad-tung <chad.tung@gmail.com>
2017-12-18 17:45:48 +0000

    created select bar

--- client/src/views/listView.js
+++ client/src/views/listView.js
@@ -8,13 +8,19 @@ var ListView = function(eventList) {
 ListView.prototype = {
   render: function(eventList){
     var eventListPage = document.getElementById('event-list-page');
+    var selectType = eLib.elementNamePlaceholderId('select', 'type');
+    var typeArr = ["UFO", "Ghost", "Cryptid", "Unidentified"];
+    typeArr.forEach(function(type) {
+      var option = eLib.elementTextIdClass('option', `${type}`);
+      selectType.appendChild(option);
+    });
+
     var eventListUL = document.createElement('ul');
     eventListPage.appendChild(eventListUL);
 
     eventList.forEach(function(event){
       eventListUL.appendChild(eLib.elementTextIdClass("li", event.title, `${event._id}`, "event-li"));
     })
-
   }
 }
 
