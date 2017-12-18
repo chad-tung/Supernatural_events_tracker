90c91319fb2f9d0e1d7f2c42e35c7414305b6e5e
Alistair Kane <vplelmo@hotmail.co.uk>
2017-12-18 17:44:31 +0000

    flexes items in listView

--- client/src/views/listView.js
+++ client/src/views/listView.js
@@ -10,11 +10,21 @@ ListView.prototype = {
     var eventListPage = document.getElementById('event-list-page');
     var eventListUL = document.createElement('ul');
     eventListUL.id = 'event-list-ul';
-    
+
     eventListPage.appendChild(eventListUL);
 
     eventList.forEach(function(event){
-      eventListUL.appendChild(eLib.elementTextIdClass("li", event.title, `${event._id}`, "event-li"));
+      var container = document.createElement('div');
+      container.classList.add("event-container");
+
+      container.appendChild(eLib.elementTextIdClass("li", event.date, `${event._id}`, "event-li"));
+      container.appendChild(eLib.elementTextIdClass("li", event.title, `${event._id}`, "event-li"));
+      var img = eLib.elementTextIdClass("img", event.image, `${event._id}`, "event-li");
+      img.src = event.image;
+      img.classList.add("event-image");
+
+      container.appendChild(img);
+      eventListUL.appendChild(container);
     })
 
   }
