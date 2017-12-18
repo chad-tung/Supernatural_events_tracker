dcae89bf72f8c97218210f72621241477ee532ea
chad-tung <chad.tung@gmail.com>
2017-12-18 13:31:39 +0000

    Got links to default images

--- server.js
+++ server.js
@@ -34,6 +34,10 @@ app.get("/api/events", function(req, res){
 	});
 });
 
+var ufoImage = "https://image.flaticon.com/icons/svg/645/645424.svg"
+var ghostImage = "https://s17.postimg.org/ebzur4svz/ghostie.png"
+var cryptidImage = "https://s17.postimg.org/lrz4d0obz/nessie.png"
+
 app.post('/event-form', function(req, res){
 	req.body.location.lat = parseFloat(req.body.location.lat);
 	req.body.location.lng = parseFloat(req.body.location.lng);
