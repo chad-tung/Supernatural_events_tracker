1b5f49a446530feb2ec9d21ddf297ca2a9fbc02e
chad-tung <chad.tung@gmail.com>
2017-12-18 13:51:42 +0000

    Set unfilled author name to Anonymous

--- server.js
+++ server.js
@@ -40,6 +40,23 @@ var cryptidImage = "https://s17.postimg.org/lrz4d0obz/nessie.png"
 var unknownImage = "https://s17.postimg.org/5waa9vanz/questionmark.png"
 
 app.post('/event-form', function(req, res){
+	if (req.body.image == "") {
+		if (req.body.type == "UFO") {
+			req.body.image = ufoImage;
+		}
+		if (req.body.type == "Cryptid") {
+			req.body.image = cryptidImage;
+		}
+		if (req.body.type == "Ghost") {
+			req.body.image = ghostImage;
+		}
+		if (req.body.type == "Unidentified") {
+			req.body.image = unknownImage;
+		}
+	}
+	if (req.body.author == "") {
+		req.body.author = "Anonymous";
+	}
 	req.body.location.lat = parseFloat(req.body.location.lat);
 	req.body.location.lng = parseFloat(req.body.location.lng);
 	db.collection("events").save(req.body, function(err, result){
