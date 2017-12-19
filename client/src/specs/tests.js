ba2fb984a630eb3b44cdd0632f495193d9c4b8a2
Alistair Kane <vplelmo@hotmail.co.uk>
2017-12-19 11:00:12 +0000

    adds selenium testing

--- client/src/specs/tests.js
+++ client/src/specs/tests.js
@@ -5,13 +5,14 @@ var expect = chai.expect;
 
 describe("site functionality", function(){
   beforeEach(function(){
+    this.timeout(5000);
     browser.ignoreSynchronisation = true;
     browser.get("http://localhost:3000");
   })
 
   it("should take you to submit on click of submit button", function(){
-    formDescription = element(by.css("#form-description"));
     element(by.css("#formButton")).click();
+    formDescription = element(by.css("#form-description"));
     expect(formDescription.getAttribute(innerText)).to.eventually.equal("Please describe what you witnessed");
   });
 
