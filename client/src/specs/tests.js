3b0952ff549610d7e8195f1370be178c5dd08d15
Alistair Kane <vplelmo@hotmail.co.uk>
2017-12-19 10:00:38 +0000

    adds a before each test

--- client/src/specs/tests.js
+++ client/src/specs/tests.js
@@ -1,3 +1,12 @@
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
+var expect = chai.expect;
+
+describe("site functionality", function(){
+  beforeEach(function(){
+    browser.ignoreSynchronisation = true;
+    browser.get("http://localhost:3000");
+  })
+
+})
